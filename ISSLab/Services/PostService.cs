57ee2be054ae31214dc83215cfa995363986ec68
dorianpop1512@gmail.com <uratenie50@gmail.com>
2024-04-08 17:08:03 +0300

    Fixed full paths

---
+++ ISSLab/Services/PostService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISSLab.Model;
+namespace ISSLab.Services
+{
+    class PostService
+    {
+        List<Post> posts;
+        public PostService()
+        {
+            posts = new List<Post>();
+        }
+        public PostService(List<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+        public void loadPostsFromDatabase()
+        {
+            //to do
+        }
+        public List<Post> GetPosts()
+        {
+               return posts;
+        }
+        public void AddPost(Post post)
+        {
+            posts.Add(post);
+        }
+        public void RemovePost(Post post)
+        {
+            posts.Remove(post);
+        }
+        public Post GetPostById(Guid id)
+        {
+            return posts.Find(post => post.Id == id);
+        }
+        public Post CreatePost(string media, Guid authorId, Guid groupId, string location, string description, string title, string contacts, string type)
+        {
+            return new Post(media, authorId, groupId, location, description, title, contacts, type);
+        }
+    }
+}
