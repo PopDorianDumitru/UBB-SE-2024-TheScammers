c4105acc8f6bbbca7ba453443c3378c6c21eccba
powervnc <marinamariaemiliana@gmail.com>
2024-04-25 14:02:34 +0300

    refactored Report

--- ISSLab/Model/Report.cs
+++ ISSLab/Model/Report.cs
@@ -11,25 +11,25 @@ namespace ISSLab.Model
         private Guid id;
         private Guid userId;
         private Guid postId;
-        private string reason;
-        private DateTime date;
+        private string reasonForReporting;
+        private DateTime dateOfReport;
 
-        public Report(Guid userId, Guid postId, string reason)
+        public Report(Guid userId, Guid postId, string reasonForReporting)
         {
             this.id = new Guid(userId.ToString() + postId.ToString());
             this.userId = userId;
             this.postId = postId;
-            this.reason = reason;
-            this.date = DateTime.Now;
+            this.reasonForReporting = reasonForReporting;
+            this.dateOfReport = DateTime.Now;
         }
 
-        public Report(Guid id, Guid userId, Guid postId, string reason, DateTime date)
+        public Report(Guid id, Guid userId, Guid postId, string reasonForReporting, DateTime dateOfReport)
         {
             this.id = id;
             this.userId = userId;
             this.postId = postId;
-            this.reason = reason;
-            this.date = date;
+            this.reasonForReporting = reasonForReporting;
+            this.dateOfReport = dateOfReport;
         }
 
         public Report()
@@ -37,14 +37,14 @@ namespace ISSLab.Model
             this.id = Guid.NewGuid();
             this.userId = Guid.NewGuid();
             this.postId = Guid.NewGuid();
-            this.reason = "";
-            this.date = DateTime.Now;
+            this.reasonForReporting = "";
+            this.dateOfReport = DateTime.Now;
         }
 
         public Guid Id { get => id; }
         public Guid UserId { get => userId; }
         public Guid PostId { get => postId; }
-        public string Reason { get => reason; set => reason = value; }
-        public DateTime Date { get => date; set => date = value; }
+        public string ReasonForReporting { get => reasonForReporting; set => reasonForReporting = value; }
+        public DateTime DateOfReport { get => dateOfReport; set => dateOfReport = value; }
     }
 }
